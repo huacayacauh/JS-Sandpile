7407b8ee7310244309835e479a5e647ad976e9fd
JeremyF <44401288+MessireToaster@users.noreply.github.com>
2020-01-21 17:08:44 +0100

    Fixed circle measures

--- js/Features/Roundness.js
+++ js/Features/Roundness.js
@@ -1,13 +1,19 @@
-var bound_set = [];
+var bound_set = [];			// Out of the circle
 var init_bounds = false;
+var anim_done = false;
+
+var round_delay = delay;
+
+var show_round = false;
 
 Tiling.prototype.get_roundness = function(){
 	
 	if(!init_bounds){ // Wait until a circle appear
+		round_delay = 0;
 		for(var i = 0; i<this.tiles.length; i++){
 			if(this.tiles[i].neighbors.length < this.tiles[i].limit){ // If tile on the ledge
 				if(this.tiles[i].sand != this.tiles[i].limit - 1){    // Not at the limit
-					return;											  // -> There is no circle yet
+					return {"Min":0, "Max":0};											  // -> There is no circle yet
 				}
 				if(!bound_set.includes(this.tiles[i].id)){
 					bound_set.push(this.tiles[i].id);                 // This tile is both on the ledge and at the limit, add it to the bound set
@@ -31,47 +37,89 @@ Tiling.prototype.get_roundness = function(){
 				}
 			}
 		}
+		// leftovers
+		var added_tiles = [];
+		do{
+			added_tiles = [];
+			for(var i = 0; i<this.tiles.length; i++){
+				if(this.tiles[i].sand == this.tiles[i].limit-1){
+					if(!bound_set.includes(this.tiles[i].id)) {											// This tile is in the disk
+						for(var j=0; j<this.tiles[i].neighbors.length; j++){
+							if(bound_set.includes(this.tiles[this.tiles[i].neighbors[j]].id)){					// This tile qualifies to be added to the bounds
+								added_tiles.push(this.tiles[i].id);											// Add it, and calculate its distance to the center of the canvas
+								break;
+							}
+						}
+					}				
+				}	
+			}
+			for(var i = 0; i<added_tiles.length; i++){
+				bound_set.push(added_tiles[i]);			// Add the new found bounds to the bounds set
+			}
+		} while(added_tiles.length >0);
+		
 		init_bounds = true;  	// reachable only when all bounds that are < limit are in bound_set
 		return; 				// next iteration, there will be circles.
 	}
 	
 	var min_radius = Infinity;
 	var max_radius = 0;
-	var current_bounds = [];
+	var added_tiles = [];
 	for(var i = 0; i<this.tiles.length; i++){
-		if(this.tiles[i].sand == this.tiles[i].limit-1 && !bound_set.includes(this.tiles[i].id)){
-			for(var j=0; j<this.tiles[i].neighbors.length; j++){
-				if(bound_set.includes(this.tiles[this.tiles[i].neighbors[j]].id)){					// This tile qualifies to be added to the bounds
-					current_bounds.push(this.tiles[i].id);											// Add it, and calculate its distance to the center of the canvas
-					for(var k = 0; k<this.tiles[i].pointsIndexes.length; k++){
-						var rad = this.points.array[this.tiles[i].pointsIndexes[k]*3]**2 + this.points.array[this.tiles[i].pointsIndexes[k]*3 +1]**2 + this.points.array[this.tiles[i].pointsIndexes[k]*3+2]**2;
-						rad = Math.sqrt(rad);
-						if(rad > max_radius){
-							max_radius = rad;
-						}
-						if(rad < min_radius){
-							min_radius = rad;
-						}
+		if(this.tiles[i].sand == this.tiles[i].limit-1){
+			if(!bound_set.includes(this.tiles[i].id)) {											// This tile is in the disk
+				for(var j=0; j<this.tiles[i].neighbors.length; j++){
+					if(bound_set.includes(this.tiles[this.tiles[i].neighbors[j]].id)){					// This tile qualifies to be added to the bounds
+						added_tiles.push(this.tiles[i].id);											// Add it, and calculate its distance to the center of the canvas
+						break;
 					}
-					break;
 				}
-			}
+			}				
 		}	
 	}
-	
-	for(var i = 0; i<current_bounds.length; i++){
-		bound_set.push(current_bounds[i]);			// Add the new found bounds to the bounds set
+	for(var i = 0; i<added_tiles.length; i++){
+		bound_set.push(added_tiles[i]);			// Add the new found bounds to the bounds set
 	}
-	if(current_bounds.length == 0){
+	var circle = []
+	for(var i = 0; i<bound_set.length; i++){	
+		for(var j=0; j<this.tiles[bound_set[i]].neighbors.length; j++){
+			if(!bound_set.includes(this.tiles[this.tiles[bound_set[i]].neighbors[j]].id)){		
+				if(show_round){
+					this.colorTile(bound_set[i], new THREE.Color(0xFF0000));	
+				}
+				circle.push(bound_set[i]);
+				break;
+			}
+		}
+	}
+	
+	for(var i = 0; i<circle.length; i++){
+		this.colorTile(circle[i], new THREE.Color(0xFF0000));
+		for(var k = 0; k<this.tiles[circle[i]].pointsIndexes.length; k++){
+			var rad = this.points.array[this.tiles[circle[i]].pointsIndexes[k]*3]**2 + this.points.array[this.tiles[circle[i]].pointsIndexes[k]*3 +1]**2 + this.points.array[this.tiles[circle[i]].pointsIndexes[k]*3+2]**2;
+			rad = Math.sqrt(rad);
+			if(rad > max_radius){
+				max_radius = rad;
+			}
+			if(rad < min_radius){
+				min_radius = rad;
+			}
+		}
+	}
+	
+	
+	if(circle.length == 0){
+		round_delay = 0;
 		return;										// No use to return anything, the circle did not change. (presumably, the tiling is stable)
 	}
 	
+	round_delay = delay;
 	return {"Min":min_radius, "Max":max_radius};
 }
 
-function makeRoundnessFile(grid){
+async function makeRoundnessFile(grid){
 	// Make a file out of the function above
-	
+	show_round = document.getElementById('roundShow').checked;
 	var arr_min = [];
 	var arr_max = [];
 	
@@ -80,6 +128,7 @@ function makeRoundnessFile(grid){
 		oldTiles.push(new Tile(grid.tiles[i].id, Array.from(grid.tiles[i].neighbors), Array.from(grid.tiles[i].pointsIndexes)));
 	}
 	var done = false;
+	round_delay = delay;
 	while(!done){
 		done = true;
 		for(var i = 0; i<grid.tiles.length; i++){
@@ -89,13 +138,21 @@ function makeRoundnessFile(grid){
 			}
 		}
 		for(var i = 0; i<50; i++){
+			if(show_round){
+				grid.colorTiles();
+			}
 			var stat = grid.get_roundness();
 			if(stat != null){
 				arr_min.push(stat["Min"]);
 				arr_max.push(stat["Max"]);
 			}
 			
-			grid.iterate();
+			for(var j = 0; j<SPEED; j++){
+				grid.iterate();
+			}
+			if(show_round){
+				await sleep(round_delay);
+			}
 		}
 	}
 	grid.colorTiles();
@@ -115,21 +172,31 @@ function makeRoundnessFile(grid){
 
     var textFile1 = window.URL.createObjectURL(data1);
 	
+	anim_done = true;
+	
 	return [textFile1]; 
 }
 
-handleDownloadRoundness = function(evt){
+handleDownloadRoundness = async function(evt){
+	document.getElementById('create3').disabled = true;
 
     if(currentGrid === undefined) return
     var link = document.getElementById('downloadlink');
+	let textFile = await makeRoundnessFile(currentGrid);
 	
-	var textFile = makeRoundnessFile(currentGrid);
+	/*while(!anim_done){
+		await(20);
+	}
+	console.log("hello");
+	anim_done = false;*/
 	
 	link.setAttribute('download', "Sandpile_Roundness.txt");
     link.href = textFile;
 	link.click();
+	
+	document.getElementById('create3').disabled = false;
 }
 
 
-var create3 = document.getElementById('create3')
+var create3 = document.getElementById('create3');
 create3.addEventListener('click', handleDownloadRoundness, false);
