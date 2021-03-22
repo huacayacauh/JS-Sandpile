98107c04155128378a423a04732703467bc20fa1
CelesteBlue-dev <celesteblue.dev@gmail.com>
2021-03-23 00:49:43 +0100

    Add good TriTriangle

--- js/TilingPresets/TriTriangleSubstitution.js
+++ js/TilingPresets/TriTriangleSubstitution.js
@@ -8,12 +8,20 @@
 // https://sites.math.washington.edu/wxml/tilings/source/Tritriangle.py
 
 //
-// [0] toolbox
+// [1] define tile types TT
 //
 
-var sqrt2 = Math.sqrt(2);
+function pushVector(array, vector) {
+  array.push(vector.x, vector.y);
+}
 
-// [I]: Define the triangle
+function triangleVectorsToBounds(v1, v2, v3) {
+  var bounds = [];
+  pushVector(bounds, v1);
+  pushVector(bounds, v2);
+  pushVector(bounds, v3);
+  return bounds;
+}
 
 function getTriBounds(size) {
   var bounds = [];
@@ -22,129 +30,161 @@ function getTriBounds(size) {
   bounds.push(size, size);
   return bounds;
 }
-var littleGreen = new Tile(["littleGreen"], [], getTriBounds(1), 3);
-var yellow = new Tile(["yellow"], [], getTriBounds(1), 3);
-var bigGreen = new Tile(["bigGreen"], [], getTriBounds(1), 3);
+var triA0 = new Tile(["triangle0"], [], getTriBounds(1), 3);
+//var triA1 = new Tile(["triangle1"], [], getTriBounds(1), 3);
+//var triA2 = new Tile(["triangle2"], [], getTriBounds(1), 3);
 
-// Conversion
+Tile.prototype.tri0to1 = function() {
+  this.id[0] = "triangle1";
+};
+Tile.prototype.tri1to2 = function() {
+  this.id[0] = "triangle2";
+};
+Tile.prototype.tri2to0 = function() {
+  this.id[0] = "triangle0";
+};
 
-Tile.prototype.littleGreenToYellow = function() {
-  this.id[0] = 'yellow';
-};
-Tile.prototype.yellowToBigGreen = function() {
-  this.id[0] = 'bigGreen';
-};
-Tile.prototype.bigGreenToLittleGreen = function() {
-	this.id[0] = "littleGreen";
+//
+// [2] define substitution TT
+//
+function substitutionTT(tile) {
+  console.log("substitutionTT: ", tile.id);
+  switch (tile.id[0]) {
+    case "triangle0":
+      /*var newtiles = [];
+      
+      var new_tri1 = tile.myclone();
+      new_tri1.tri0to1();
+      new_tri1.id.push("fils_0to1");
+      newtiles.push(new_tri1);
+      
+      return newtiles;*/
+      tile.tri0to1();
+      tile.id.push("fils_0to1");
+      return tile;
+      break;
+    case "triangle1":
+      /*var newtiles = [];
+      
+      var new_tri2 = tile.myclone();
+      new_tri2.tri1to2();
+      new_tri2.id.push("fils_1to2");
+      newtiles.push(new_tri2);
+      
+      return newtiles;*/
+      tile.tri1to2();
+      tile.id.push("fils_1to2");
+      return tile;
+      break;
+    case "triangle2":
+      var newtiles = [];
+      
+      var A = new THREE.Vector2(tile.bounds[0], tile.bounds[1]);
+      var B = new THREE.Vector2(tile.bounds[2], tile.bounds[3]);
+      var C = new THREE.Vector2(tile.bounds[4], tile.bounds[5]);
+      var AB_center = ((new THREE.Vector2()).addVectors(A, B)).divideScalar(2);
+      var AC_center = ((new THREE.Vector2()).addVectors(A, C)).divideScalar(2);
+      var BC_center = ((new THREE.Vector2()).addVectors(B, C)).divideScalar(2);
+      var AC_quarter = ((((new THREE.Vector2()).add(A)).multiplyScalar(3)).add(C)).divideScalar(4);
+      var ABC_center = ((new THREE.Vector2()).addVectors(AC_center, B)).divideScalar(2);
+      
+      var new_tri2 = tile.myclone();
+      new_tri2.bounds = triangleVectorsToBounds(C, AC_center, B);
+      new_tri2.id.push("fils0");
+      new_tri2.sand = 0;
+      newtiles.push(new_tri2);
+      
+      var new_tri0_0 = tile.myclone();
+      new_tri0_0.tri2to0();
+      new_tri0_0.id.push("fils1");
+      new_tri0_0.bounds = triangleVectorsToBounds(AB_center, AC_quarter, A);
+      new_tri0_0.sand = 1;
+      console.log(new_tri0_0);
+      newtiles.push(new_tri0_0);
+
+      var new_tri0_1 = tile.myclone();
+      new_tri0_1.tri2to0();
+      new_tri0_1.id.push("fils2");
+      new_tri0_1.bounds = triangleVectorsToBounds(B, ABC_center, AB_center);
+      new_tri0_1.sand = 2;
+      newtiles.push(new_tri0_1);
+      
+      var new_tri0_2 = tile.myclone();
+      new_tri0_2.tri2to0();
+      new_tri0_2.id.push("fils3");
+      new_tri0_2.bounds = triangleVectorsToBounds(AC_center, AC_quarter, AB_center);
+      new_tri0_2.sand = 3;
+      newtiles.push(new_tri0_2);
+      
+      var new_tri0_3 = tile.myclone();
+      new_tri0_3.tri2to0();
+      new_tri0_3.id.push("fils4");
+      new_tri0_3.bounds = triangleVectorsToBounds(AC_center, ABC_center, AB_center);
+      new_tri0_3.sand = 4;
+      newtiles.push(new_tri0_3);
+      
+      /*
+      function getType(A, B, C) {
+          if (A.x = C.x && A.y > C.y) {
+              if (A.x > B.x)
+                return 0;
+              else
+                return 1;
+          else if (A.x > C.x) {
+              if (Math.max(A.y, C.y) < B.y)
+                return 0;
+              else
+                return 1;
+          } else {
+              if ((A.y, C.y)/2 > B.y)
+                return 0;
+              else
+                return 1;
+          }
+      }
+      console.log(getType(AC_center, AC_quarter, AB_center));
+      console.log(getType(AC_center, ABC_center, AB_center));
+      */
+      
+      return newtiles;
+      break;
+    default:
+      console.log("caution: undefined tile type for substitutionTT, id = " + tile.id);
+  }
 }
 
-// [II]: Define Substitution 
-
-function substitutionTriTriangle(tile) {
-	switch (tile.id[0]) {
-		case "littleGreen":
-			tile.littleGreenToYellow();
-			return tile;
-			break;
-		case "yellow":
-			tile.yellowToBigGreen();
-			return tile;
-			break;
-		case "bigGreen":
-			var newtiles = [];
-
-			var lenghtTriTriangle = tile.bounds[2] - tile.bounds[0]
-			
-			var littleGreen1 = tile.myclone();
-			littleGreen1.bigGreenToLittleGreen();
-			littleGreen1.id.push("littleGreen1");
-			littleGreen1.bounds[0] = tile.bounds[2] + 1/2*(tile.bounds[4] - tile.bounds[2]); // Simplifié
-			littleGreen1.bounds[1] = tile.bounds[3] + 1/2*(tile.bounds[5] - tile.bounds[3]); // Simplifié
-			littleGreen1.bounds[2] = tile.bounds[0] + 3/4* (tile.bounds[4] - tile.bounds[0]); 
-			littleGreen1.bounds[3] = tile.bounds[1] + 3/4*(tile.bounds[5] - tile.bounds[1]);
-			littleGreen1.bounds[4] = tile.bounds[4];
-			littleGreen1.bounds[5] = tile.bounds[5];
-			newtiles.push(littleGreen1);
-			
-			var littleGreen4 = littleGreen1.myclone();
-			littleGreen4.id.push("littleGreen4");
-			littleGreen4.shift((tile.bounds[2] - tile.bounds[4])/2,(tile.bounds[3] - tile.bounds[5])/2);
-			
-			var littleGreen2 = littleGreen1.myclone();
-			littleGreen2.id.push("littleGreen2"); // Change ID 
-			littleGreen2.bounds[0] = tile.bounds[0] + 1/2* (tile.bounds[4] - tile.bounds[0]);
-			littleGreen2.bounds[1] = tile.bounds[1] + 1/2*(tile.bounds[5] - tile.bounds[1]);
-			littleGreen2.bounds[2] = tile.bounds[0] + 3/4* (tile.bounds[4] - tile.bounds[0]);
-			littleGreen2.bounds[3] = tile.bounds[1] + 3/4*(tile.bounds[5] - tile.bounds[1]);
-			littleGreen2.bounds[4] = littleGreen1.bounds[0];
-			littleGreen2.bounds[5] = littleGreen1.bounds[1];
-
-			newtiles.push(littleGreen2);
-			
-			var littleGreen3 = littleGreen2.myclone();
-			littleGreen3.id.push("littleGreen3");
-			littleGreen3.bounds[0] = littleGreen2.bounds[0];
-			littleGreen3.bounds[1] = littleGreen2.bounds[1];
-			littleGreen3.bounds[2] = littleGreen4.bounds[2];
-			littleGreen3.bounds[3] = littleGreen4.bounds[3];
-			littleGreen3.bounds[4] = littleGreen1.bounds[0];
-			littleGreen3.bounds[5] = littleGreen1.bounds[1];
-			newtiles.push(littleGreen3);
-			
-			newtiles.push(littleGreen4);
-			
-			var newBigGreen = tile.myclone();// No duplication, we reuse the older triangle
-			newBigGreen.id.push("bigGreen");
-			newBigGreen.bounds[0] = tile.bounds[0];
-			newBigGreen.bounds[1] = tile.bounds[1];
-			newBigGreen.bounds[2] = littleGreen2.bounds[0];
-			newBigGreen.bounds[3] = littleGreen2.bounds[1];
-			newBigGreen.bounds[4] = tile.bounds[2];
-			newBigGreen.bounds[5] = tile.bounds[3];
-			//newBigGreen.shift(lenghtTriTriangle, lenghtTriTriangle);
-			newtiles.push(newBigGreen);
-			
-			return newtiles;
-			break;
-		default:		
-			console.log("caution: undefined tile type for substitutionP2, id="+tile.id);
-			break;
-	}
-}
-
-// [3] no duplication
+//
+// [3] no duplicated tiles
 // [4] I am lazy
+//
 
+//
+// [6] use default neighbors2bounds
+//
 
-// prepare decoration
-decorateTriTriangle = new Map();
-decorateTriTriangle.set('littleGreen',0);
-decorateTriTriangle.set('yellow',1);
-decorateTriTriangle.set('bigGreen',2);
-decorateTriTriangle.set('littleGreen1',3);
-// [6]
+var neighbors2boundsTT = new Map();
+neighbors2boundsTT.set("triangle0", default_neighbors2bounds(3));
+neighbors2boundsTT.set("triangle1", default_neighbors2bounds(3));
+neighbors2boundsTT.set("triangle2", default_neighbors2bounds(3));
 
-var neighbors2boundsTriTriangle = new Map();
-neighbors2boundsTriTriangle.set('littleGreen', default_neighbors2bounds(3));
-neighbors2boundsTriTriangle.set('yellow', default_neighbors2bounds(3));
-neighbors2boundsTriTriangle.set('bigGreen', default_neighbors2bounds(3));
-
+//
 // [7] construct base tilings and call substitute
+//
 
-Tiling.TriTriangleBySubst = function({iterations}={}){
+Tiling.TriTriangleBySubst = function({iterations}={}) {
   var tiles = [];
-  var theLittleGreen = littleGreen.myclone();
-  tiles.push(theLittleGreen);
+  var my_tri = triA0.myclone();
+  tiles.push(my_tri);
   tiles = substitute(
     iterations,
     tiles,
-    1,
-    substitutionTriTriangle,
+    Math.sqrt(2),
+    substitutionTT,
     [], // no duplicated tiles
     [], // no duplicated tiles
     "laziness", // myneighbors
-    neighbors2boundsTriTriangle,
-    decorateTriTriangle // decorateTT
+    neighbors2boundsTT,
+    false//decorateTT
   );
   return new Tiling(tiles);
-}
+};
