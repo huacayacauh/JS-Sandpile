3ba327a0dc85bd08068cd5ee4c0b7c541a5fb9a4
Pachumain <Azertyque96>
2021-04-27 13:17:53 +0200

    Voisinage hyperbol

--- js/TilingPresets/Hyperbol2Tiling.js
+++ js/TilingPresets/Hyperbol2Tiling.js
@@ -1,46 +1,5 @@
-/*
-
-Queue.js
-
-A function to represent a queue
-
-Created by Kate Morley - http://code.iamkate.com/ - and released under the terms
-of the CC0 1.0 Universal legal code:
-
-http://creativecommons.org/publicdomain/zero/1.0/legalcode
-
-*/
-
-/* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
-* items are added to the end of the queue and removed from the front.
-*/
-function Liste(){
-
-  var list  = [];
-  var offset = 0;
-
-  this.getLength = function(){
-    return (list.length - offset);
-  }
-
-  this.isEmpty = function(){
-    return (list.length == 0);
-  }
-
-
-  this.push_back = function(item){
-    list.push(item);
-  }
-  this.push_front = function(item){
-    list.unshift(item);
-  }
-
-  this.pop_front = function(item){
-    return list.shift(item);
-  }
-
-
-}
+const countOccurrences = (arr, val) => arr.reduce((a, v) =>
+(typeof v == 'string' ? v === val ? a + 1 : a : a+countOccurrences(v,val)), 0);
 
 function reorder (bounds, B0x, B0y, B1x, B1y){
   var new_bounds = [];
@@ -114,6 +73,7 @@ function reorder (bounds, B0x, B0y, B1x, B1y){
   return new_bounds;
 }
 
+
 function calcul_voisin (p,q){
 
   var voisin_depart = ['N'];
@@ -121,27 +81,24 @@ function calcul_voisin (p,q){
   var voisin_p = [['N','P'],['N']];
 
 
-  for (var h = 2; h < q; h++){
-      voisin_depart = voisin_depart.concat(['P'])
+  for (var h = 4; h <= p; h++){
+    voisin_depart = voisin_depart.concat(['P'])
   }
 
   for (var i = 4; i < q; i++){
-      voisin_n = [['N','P']].concat(voisin_n)
-      voisin_p = [['N','P']].concat(voisin_p)
+    voisin_n = [['N','P']].concat(voisin_n)
+    voisin_p = [['N','P']].concat(voisin_p)
   }
   for (var j = 4; j < p; j++){
     for (var k = 0; k < voisin_n.length; k++){
-        voisin_n[k] = voisin_n[k].concat('P');
+      voisin_n[k] = voisin_n[k].concat('P');
     }
     for (var k = 0; k < voisin_p.length; k++){
       voisin_p[k] = voisin_p[k].concat('P');
     }
   }
-
-console.log( [voisin_depart,voisin_n,voisin_p]);
-
+  console.log([voisin_depart,voisin_n,voisin_p]);
   return [voisin_depart,voisin_n,voisin_p];
-
 
 }
 
@@ -149,9 +106,14 @@ console.log( [voisin_depart,voisin_n,voisin_p]);
 
 function make_hyperbol2tiling(p, q, star, iter_num, Ox, Oy, R) {
   var tiles = [];
-  var tiles_liste = new Liste();
+  var tiles_liste = [];
   var dict = [];
   var id = 0;
+  var idmax = []
+  var iter_cur = 0;
+  var cpt_n = 0;
+  var cpt_t = 0;
+  var id_facteur =0;
 
   // Verification des parametres
   if (1/p + 1/q >= 1/2) {
@@ -173,6 +135,20 @@ function make_hyperbol2tiling(p, q, star, iter_num, Ox, Oy, R) {
   var voisin_n ;
   var voisin_p ;
   [voisin_depart,voisin_n,voisin_p] = calcul_voisin(p,q)
+  var nombre_n = q * countOccurrences(voisin_depart,'N');
+  var nombre_p = q * countOccurrences(voisin_depart,'P');
+  id_facteur = (nombre_n*voisin_n.flat().length+nombre_p*voisin_p.flat().length)/(nombre_n+nombre_p)
+console.log(id_facteur);
+  idmax.push(1);
+
+  for (var k =1; k<= iter_num;k++){
+    var nb_edge = nombre_n+nombre_p;
+    idmax.push(nb_edge);
+    [nombre_n,nombre_p] =
+    [nombre_n*countOccurrences(voisin_n,'N')+nombre_p*countOccurrences(voisin_p,'N'),
+    nombre_n*countOccurrences(voisin_n,'P')+nombre_p*countOccurrences(voisin_p,'P')];
+  }
+  console.log(idmax);
 
   // Creation des tuiles de base
   if (!star) {
@@ -184,28 +160,22 @@ function make_hyperbol2tiling(p, q, star, iter_num, Ox, Oy, R) {
     }
     //console.log(bounds);
     var neighbour_by_side = [];
-    for (var k = 0; k < bounds.length / 2; k++) {
-      neighbour_by_side.push(undefined);
-      var point = PoincareTilingPoint(bounds[k*2], bounds[k*2+1], 0);
-      arrIncr(dict, point);
+    for (var k = 0; k < q; k++) {
+      neighbour_by_side.push([k*voisin_depart.length,1]);
     }
     //console.log(dict);
-    var tile = new Tile([0, 0], [], bounds, q);
+    var tile = new Tile([0, 0], neighbour_by_side, bounds, q);
     //  tile.neighbour_by_side = neighbour_by_side;
     tiles.push(tile);
     if (iter_num>0){
-  //     tiles_liste.push_back([tile,0,p-3,1,0]);
- // tiles_liste.push_back([tile,0,['N','P','P'],1]);
-  // tiles_liste.push_back([tile,2,['N','P','P'],1]);
-
-     for (var side = 0 ; side < q; side++){
-      tiles_liste.push_back([tile,side,[].concat(voisin_depart),1]);
-     }
+      for (var side = 0 ; side < q; side++){
+        tiles_liste.push([tile,side,[].concat(voisin_depart),1]);
+      }
     }
   }
 
 
-  while ( !tiles_liste.isEmpty())
+  while ( tiles_liste.length != 0 )
   {
     var tile ;
     var side_no;
@@ -213,7 +183,7 @@ function make_hyperbol2tiling(p, q, star, iter_num, Ox, Oy, R) {
     var iter ;
     var fill ;
 
-    [tile,side_no,pile,iter,fill] = tiles_liste.pop_front();
+    [tile,side_no,pile,iter,fill] = tiles_liste.shift();
 
 
     var side_num = tile.bounds.length / 2;
@@ -225,57 +195,89 @@ function make_hyperbol2tiling(p, q, star, iter_num, Ox, Oy, R) {
       tile.bounds[(2*side_no+3)%(side_num*2)],
       Ox, Oy, R);
 
-      // console.log(ret);
-
 
       var T_2_bounds = circle_inversion_polygon(tile.bounds, ret.Cx, ret.Cy, ret.r);
-//      console.log(  tile.bounds[(2*side_no+0)%(side_num*2)],
-//         tile.bounds[(2*side_no+1)%(side_num*2)],
-//         tile.bounds[(2*side_no+2)%(side_num*2)],
-//         tile.bounds[(2*side_no+3)%(side_num*2)]);
 
+      // console.log(T_2_bounds);
 
-
-// console.log(T_2_bounds);
-
-     T_2_bounds =  reorder(T_2_bounds,
+      T_2_bounds =  reorder(T_2_bounds,
         tile.bounds[(2*side_no+2)%(side_num*2)],
         tile.bounds[(2*side_no+3)%(side_num*2)],
         tile.bounds[(2*side_no+0)%(side_num*2)],
         tile.bounds[(2*side_no+1)%(side_num*2)]
       );
-     // console.log(T_2_bounds);
+      var fill = pile.shift();
+
+      var neighbour_by_side = [];
+
+      if (iter_cur != iter){
+        id = 0;
+        cpt_n=0;
+        cpt_p=0;
+        iter_cur = iter;
+        neighbour_by_side.push([idmax[iter]-1,iter]);
+      }else{
+        neighbour_by_side.push([id-1,iter]);
+      }
+      if (id == idmax[iter_cur]-1 ){
+
+        neighbour_by_side.push([0,iter]);
+      }else{
+        neighbour_by_side.push([id+1,iter]);
+      }
+
+      var id_fils = cpt_n*voisin_n.flat().length+cpt_p*voisin_p.flat().length;
+      neighbour_by_side.push([id_fils,iter+1]);
+      if (fill == 'N'){
+        neighbour_by_side.push(tile.id);
+
+        for (var k = 0; k < voisin_n.length-1; k++){
+          id_fils = id_fils+ voisin_n[k].length;
+          neighbour_by_side.push([id_fils,iter+1]);
+
+        }
+      }else{
+        for (var k = 0; k < voisin_p.length-1; k++){
+          id_fils = id_fils+ voisin_p[k].length;
+          neighbour_by_side.push([id_fils,iter+1]);
+
+        }
+      }
 
 
-      var T_2 = new Tile([id], [], T_2_bounds, q);
+
+      var T_2 = new Tile([id,iter_cur], neighbour_by_side, T_2_bounds, q);
 
 
       id = id+1;
+      if (fill == 'N'){
+        cpt_n = cpt_n+1;
+      }else{
+        cpt_p = cpt_p+1;
+      }
       tiles.push(T_2);
 
-    //  console.log("cpt: ",cpt, "  | iter: ",iter, " | d: ",d);
+      //  console.log("cpt: ",cpt, "  | iter: ",iter, " | d: ",d);
       // console.log(pile);
-      var fill = pile.shift();
       // console.log(pile);
 
       if (pile.length > 0){
         // if (fill == 'N'){
-          tiles_liste.push_front([T_2, q-1 ,pile,iter]);
-        }
-
-      if (iter < iter_num){
-          if (fill == 'N'){
-            for (var k = 0; k < voisin_n.length; k++){
-              tiles_liste.push_back([T_2, 2 + k ,[].concat(voisin_n[k]) ,iter+1]);
-            }
-          }else{
-            for (var k = 0; k < voisin_p.length; k++){
-              tiles_liste.push_back([T_2, 1 + k ,[].concat(voisin_p[k]) ,iter+1]);
-            }
-          }
+        tiles_liste.unshift([T_2, q-1 ,pile,iter]);
       }
 
-  }
+      if (iter < iter_num){
+        if (fill == 'N'){
+          for (var k = 0; k < voisin_n.length; k++){
+            tiles_liste.push([T_2, 2 + k ,[].concat(voisin_n[k]) ,iter+1]);
+          }
+        }else{
+          for (var k = 0; k < voisin_p.length; k++){
+            tiles_liste.push([T_2, 1 + k ,[].concat(voisin_p[k]) ,iter+1]);
+          }
+        }
+      }
+    }
 
     tiles.forEach(tile => tile.scale(0,0,200))
 
