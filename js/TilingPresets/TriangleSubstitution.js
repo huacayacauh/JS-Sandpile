6d8cd69c838c9a9d269918736719225a18a1c9d2
Auhalf <leah.tapin@orange.fr>
2021-04-27 11:50:54 +0200

    déplacement de findNeighbors dans Substitution API + correction des TriangleSubstitution (Triangle Incenter) pour revenir à findNeighborsEdgeToEdge

--- js/TilingPresets/TriangleSubstitution.js
+++ js/TilingPresets/TriangleSubstitution.js
@@ -15,10 +15,10 @@ bounds.push(0, Math.sqrt(0.75));  // equilateral
 var tri = new Tile(['triangle'],[],bounds,3);
 
 //
-// [2] define substitution 
+// [2] define substitution
 //
 function incenterSubstitution(tile){
-	
+
 	// Calculate midpoint of each segment
 	var midpointABx = (tile.bounds[0] + tile.bounds[2])/2;
 	var midpointABy = (tile.bounds[1] + tile.bounds[3])/2;
@@ -28,68 +28,68 @@ function incenterSubstitution(tile){
 
 	var midpointCAx = (tile.bounds[0] + tile.bounds[4])/2;
 	var midpointCAy = (tile.bounds[1] + tile.bounds[5])/2;
-	
+
 	// Calculate edges lengths in order to calculate the triangle incenter
 	// Length are labeled according to the opposite vertex
-	
+
 	var lengthC = Math.sqrt(Math.pow(tile.bounds[0] - tile.bounds[2], 2) + Math.pow(tile.bounds[1] - tile.bounds[3], 2));
 	var lengthA = Math.sqrt(Math.pow(tile.bounds[2] - tile.bounds[4], 2) + Math.pow(tile.bounds[3] - tile.bounds[5], 2));
 	var lengthB = Math.sqrt(Math.pow(tile.bounds[0] - tile.bounds[4], 2) + Math.pow(tile.bounds[1] - tile.bounds[5], 2));
-	
+
 	// Calculate triangle incenter
-	
+
 	var incenterx = tile.bounds[0] * lengthA + tile.bounds[2] * lengthB + tile.bounds[4] * lengthC;
 	var incentery = tile.bounds[1] * lengthA + tile.bounds[3] * lengthB + tile.bounds[5] * lengthC;
-	
-	
+
+
 	incenterx = incenterx / (lengthA + lengthB + lengthC);
 	incentery = incentery / (lengthA + lengthB + lengthC);
-	
-	// builds ids 
-	
+
+	// builds ids
+
 	var ids = [];
 	for(var i = 0; i<6; i++){
 		var idi = tile.id.slice();
 		idi.push(i);
 		ids.push(idi);
 	}
-	
+
 	// build bounds
 	// could have probably made a smart loop
-	
+
 	var bounds0 = [];
 	bounds0.push(tile.bounds[0], tile.bounds[1]);
 	bounds0.push(midpointABx, midpointABy);
 	bounds0.push(incenterx, incentery);
-	
+
 	var bounds1 = [];
 	bounds1.push(midpointABx, midpointABy);
 	bounds1.push(tile.bounds[2], tile.bounds[3]);
 	bounds1.push(incenterx, incentery);
-	
+
 	var bounds2 = [];
 	bounds2.push(tile.bounds[2], tile.bounds[3]);
 	bounds2.push(incenterx, incentery);
 	bounds2.push(midpointBCx, midpointBCy);
-	
+
 	var bounds3 = [];
 	bounds3.push(midpointBCx, midpointBCy);
 	bounds3.push(incenterx, incentery);
 	bounds3.push(tile.bounds[4], tile.bounds[5]);
-	
+
 	var bounds4 = [];
 	bounds4.push(tile.bounds[4], tile.bounds[5]);
 	bounds4.push(incenterx, incentery);
 	bounds4.push(midpointCAx, midpointCAy);
-	
+
 	var bounds5 = [];
 	bounds5.push(tile.bounds[0], tile.bounds[1]);
 	bounds5.push(midpointCAx, midpointCAy);
 	bounds5.push(incenterx, incentery);
-	
-	
-	
-	
+
+
+
+
 
 	// tile <-> id, neighbors, bounds, topple limit
 	t0 = new Tile(ids[0], [], bounds0, 3);
@@ -98,8 +98,8 @@ function incenterSubstitution(tile){
 	t3 = new Tile(ids[3], [], bounds3, 3);
 	t4 = new Tile(ids[4], [], bounds4, 3);
 	t5 = new Tile(ids[5], [], bounds5, 3);
-	
-	
+
+
 	// add sand to ease the visualization of the subsitution
 	t0.sand = 0;
 	t1.sand = 1;
@@ -107,17 +107,17 @@ function incenterSubstitution(tile){
 	t3.sand = 3;
 	t4.sand = 4;
 	t5.sand = 5;
-	
+
 	// build tiles
 	var newtiles = [];
-	
+
 	newtiles.push(t0);
 	newtiles.push(t1);
 	newtiles.push(t2);
 	newtiles.push(t3);
 	newtiles.push(t4);
 	newtiles.push(t5);
-	
+
 	return newtiles;
 
 }
@@ -129,7 +129,7 @@ function incenterSubstitution(tile){
 
 //
 // [6] use default neighbors2bounds
-// 
+//
 var neighbors2boundsTri = new Map();
 neighbors2boundsTri.set('triangle',default_neighbors2bounds(3));
 
@@ -140,7 +140,7 @@ neighbors2boundsTri.set('triangle',default_neighbors2bounds(3));
 
 //
 // [7.1] construct "Triangle Incenter" tiling by substitution
-// 
+//
 Tiling.TriangleIncenter = function({iterations}={}){
   // push base tiling
   var tiles = [];
@@ -157,7 +157,7 @@ Tiling.TriangleIncenter = function({iterations}={}){
     incenterSubstitution,
     [], // no duplicated tiles
     [], // no duplicated tiles
-    "I am lazy", // myneighbors
+    "edge-to-edge", // myneighbors
     neighbors2boundsTri,
     false,
   );
@@ -169,7 +169,7 @@ Tiling.TriangleIncenter = function({iterations}={}){
 
 // This substitution was inspired by Butler, Steve, and Ron Graham.
 // "Subdivision by bisectors is dense in the space of all triangles.", 2010
-//	
+//
 // triangles renders one in front of the other, but here is the original subsitution
 //
 // var bounds0 = [];
