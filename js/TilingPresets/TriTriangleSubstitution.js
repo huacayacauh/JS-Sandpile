d825c69c0d5c2b3e74597e6a7e249da38ad30cbc
Pachumain <Azertyque96>
2021-05-10 18:03:24 +0200

    SubstituteGeneral -> Substitute

--- js/TilingPresets/TriTriangleSubstitution.js
+++ js/TilingPresets/TriTriangleSubstitution.js
@@ -52,25 +52,25 @@ function substitutionTT(tile) {
   switch (tile.id[0]) {
     case "triangle0":
       var newtiles = [];
-      
+
       var new_tri1 = tile.myclone();
       new_tri1.tri0to1();
       new_tri1.id.push("fils_0to1");
       newtiles.push(new_tri1);
-      
+
       return newtiles;
     case "triangle1":
       var newtiles = [];
-      
+
       var new_tri2 = tile.myclone();
       new_tri2.tri1to2();
       new_tri2.id.push("fils_1to2");
       newtiles.push(new_tri2);
-      
+
       return newtiles;
     case "triangle2":
       var newtiles = [];
-      
+
       var A = new THREE.Vector2(tile.bounds[0], tile.bounds[1]);
       var B = new THREE.Vector2(tile.bounds[2], tile.bounds[3]);
       var C = new THREE.Vector2(tile.bounds[4], tile.bounds[5]);
@@ -79,13 +79,13 @@ function substitutionTT(tile) {
       var BC_center = ((new THREE.Vector2()).addVectors(B, C)).divideScalar(2);
       var AC_quarter = ((((new THREE.Vector2()).add(A)).multiplyScalar(3)).add(C)).divideScalar(4);
       var ABC_center = ((new THREE.Vector2()).addVectors(AC_center, B)).divideScalar(2);
-      
+
       var new_tri2 = tile.myclone();
 			new_tri2.limit = 3
       new_tri2.bounds = triangleVectorsToBounds(C, AC_center, B);
       new_tri2.id.push("fils0");
       newtiles.push(new_tri2);
-      
+
       var new_tri0_0 = tile.myclone();
       new_tri0_0.tri2to0();
       new_tri0_0.id.push("fils1");
@@ -97,19 +97,19 @@ function substitutionTT(tile) {
       new_tri0_1.id.push("fils2");
       new_tri0_1.bounds = triangleVectorsToBounds(B, ABC_center, AB_center);
       newtiles.push(new_tri0_1);
-      
+
       var new_tri0_2 = tile.myclone();
       new_tri0_2.tri2to0();
       new_tri0_2.id.push("fils3");
       new_tri0_2.bounds = triangleVectorsToBounds(AC_center, AC_quarter, AB_center);
       newtiles.push(new_tri0_2);
-      
+
       var new_tri0_3 = tile.myclone();
       new_tri0_3.tri2to0();
       new_tri0_3.id.push("fils4");
       new_tri0_3.bounds = triangleVectorsToBounds(AC_center, ABC_center, AB_center);
       newtiles.push(new_tri0_3);
-      
+
       return newtiles;
     default:
       console.log("caution: undefined tile type for substitutionTT, id = " + tile.id);
@@ -144,7 +144,7 @@ Tiling.TriTriangleBySubst = function({iterations}={}) {
   var tiles = [];
   var my_tri = triA0.myclone();
   tiles.push(my_tri);
-  tiles = substituteGeneral(
+  tiles = substitute(
     iterations,
     tiles,
     Math.sqrt(2),
@@ -157,5 +157,3 @@ Tiling.TriTriangleBySubst = function({iterations}={}) {
   );
   return new Tiling(tiles);
 };
-
-
