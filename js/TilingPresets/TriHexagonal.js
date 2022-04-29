05d42536815e4a08ea5ea735c22630ce405ababd
Nino <dauvier.nino@gmail.com>
2022-04-29 17:19:36 +0200

    version finale de dual tilling et TriHexagonal fonctionnel. Attention peut être à un conflit entre triHexagonal et Hexagonal(je ne sais pas si c est seulment ma version de Hex qui ne marche pas ou si c est autre chose)

--- js/TilingPresets/TriHexagonal.js
+++ js/TilingPresets/TriHexagonal.js
@@ -1,12 +1,28 @@
 Tiling.triHexTiling = function ({size}={}) {
 
 	var tils = [];
+	var parcouru = []
+	var compteur = true
 	
 	for(var x=-size; x<=size; x++){
 		for(var y=-size; y<=size; y++){
 			for(var z=-size; z<=size; z++){
 				if(x+y+z == 0){
-					tils.push(hexTile(x, y, z));
+					last_tile = hexTile(3*x, 3*y, 3*z);
+					tils.push(last_tile);
+					last_tile.neighbors.forEach(function(pos){
+						var free = true;
+						parcouru.forEach(function(other_pos){
+							if (pos[0]==other_pos[0] && pos[1]==other_pos[1]){
+								free = false;
+							}
+						})
+						if (free == true  && -3*size-1<=pos[0] && -3*size-1<=pos[1] && -3*size-1<=pos[2] && pos[0]<=3*size+1 && pos[1]<=3*size+1 && pos[2]<=3*size+1){
+							tils.push(triTile(pos[0],pos[1], pos[2], compteur));
+							parcouru.push(pos);
+						}
+						compteur = 1 - compteur
+					});
 				}
 			}
 		}
@@ -15,27 +31,61 @@ Tiling.triHexTiling = function ({size}={}) {
 	return new Tiling(tils);
 }
 
-function hexTile(x, y, z){
+function triTile(x, y, z, bool){
 	var id = [x, y, z];
 	
 	var neighbors =  [];
-	neighbors.push([x-1, y,   z+1]);
-	neighbors.push([x-1, y+1, z  ]);
-	neighbors.push([x,   y+1, z-1]);
-	neighbors.push([x,   y-1, z+1]);
-	neighbors.push([x+1, y,   z-1]);
-	neighbors.push([x+1, y-1, z  ]);
+	if (bool == false){
+		neighbors.push([x - 1, y - 1, z + 2]);
+		neighbors.push([x - 1, y + 2, z - 1]);
+		neighbors.push([x + 2, y - 1, z - 1]);
+	}
+	else{
+		neighbors.push([x + 1, y - 2, z + 1]);
+		neighbors.push([x - 2, y + 1, z + 1]);
+		neighbors.push([x + 1, y + 1, z - 2]);
+	}
 	
 	let sq3 = Math.sqrt(3)/2;
+	let invsq3 = 1/Math.sqrt(3)
 	var bounds = [];
-	bounds.push(1.5*x-0.5, (y-z-1)*sq3);
-	bounds.push(1.5*x-1, (y-z)*sq3);
-	bounds.push(1.5*x-0.5, (y-z+1)*sq3);
-	bounds.push(1.5*x+0.5, (y-z+1)*sq3);
-	bounds.push(1.5*x+1, (y-z)*sq3);
-	bounds.push(1.5*x+0.5, (y-z-1)*sq3);
+	if (bool == true){
+		bounds.push(2*sq3*x - 0.5*invsq3*3, (y-z) - 0.5*3);
+		bounds.push(2*sq3*x + invsq3*3, (y-z));
+		bounds.push(2*sq3*x  - 0.5*invsq3*3, (y-z) + 0.5*3);
+	}
+	else{
+		bounds.push(2*sq3*x + 0.5*invsq3*3, (y-z) - 0.5*3);
+		bounds.push(2*sq3*x - invsq3*3, (y-z));
+		bounds.push(2*sq3*x  + 0.5*invsq3*3, (y-z) + 0.5*3);		
+	}
 	
-	return new Tile(id, neighbors, bounds, 6);
+	return new Tile(id, neighbors, bounds, 3);
 }
 
+function hexTile(x, y, z){
+	var id = [x, y, z];
 
+	var neighbors =  [];
+	neighbors.push([x - 1, y - 1, z + 2]);
+	neighbors.push([x + 1, y - 2, z + 1]);
+	neighbors.push([x - 1, y + 2, z - 1]);
+	neighbors.push([x - 2, y + 1, z + 1]);
+	neighbors.push([x + 2, y - 1, z - 1]);
+	neighbors.push([x + 1, y + 1, z - 2]);
+	
+
+	let sq3 = Math.sqrt(3)/2;
+	var bounds = [];
+	bounds.push(2*sq3*x - sq3*3, (y-z) - 0.5*3);
+	bounds.push(2*sq3*x , (y-z) - 3);
+	bounds.push(2*sq3*x + sq3*3, (y-z) - 0.5*3);
+	bounds.push(2*sq3*x + sq3*3, (y-z) + 0.5*3);
+	bounds.push(2*sq3*x , (y-z) + 3);
+	bounds.push(2*sq3*x - sq3*3, (y-z) + 0.5*3);
+	
+	return new Tile(id, neighbors, bounds, 6);
+
+
+
+}
---
+++ js/Utils/dualTiling.js
@@ -0,0 +1,163 @@
+function dualTiling(tiling){
+	//crée le pavage dual associé à tiling
+	let newTils = [];
+	//marg erreur sert à éviter les erreurs d'arrondi entre les bordures du pavage initiale
+  let margeErreur = dualDistance(coordVertex(tiling, 0, 0), coordVertex(tiling, 0, 1))/30;
+  
+  let newBounds = createNewBounds(tiling);
+	//positionToId est un dictionnaire qui renvoie les sommet de chaque tuile à l'id qui est associé(tout les sommts qui se touchent ont le meme id)
+	let positionToId = new Map();
+	createId(tiling, margeErreur, positionToId);
+	
+  let parcouru = [];
+  for(var i=0; i<tiling.tiles.length; i++){
+    for(var j=0; j<tiling.tiles[i].bounds.length/2; j++){
+    	if (nonParcouru(i, j, parcouru, tiling, margeErreur)){
+	      let lastTile = -1;
+	      let currentTile = i;
+	      let nextTile = 0;
+				let currentVertexIndex = j;
+	      let bounds = [];
+	      let currentNeighbor = -1;
+	      let neighbors = [];
+
+				//on cherche a faire le tour du sommet j de la tuile i en retenant les tuiles traversés
+				searchCompleted = findNextTile(tiling, newBounds, i, currentTile, currentVertexIndex, lastTile, nextTile, bounds, neighbors, currentNeighbor, margeErreur, positionToId);
+									
+				// si on a bien fait le tour
+				if (searchCompleted){
+					parcouru.push(coordVertex(tiling, i, j));
+	  		  newTils.push(new Tile(positionToId.get(i.toString()+","+ j.toString()), neighbors, bounds, neighbors.length));
+				}
+      }
+    }
+  }
+  console.log("END construct the dual Tiling");
+  console.log("INFO the current Tiling has "+newTils.length+" tiles");
+  return new Tiling(newTils);
+}
+
+//findNextTile est une fonction récurssive qui cherche le voisin de la tuile currentTile qui contient le sommet initiale et le dernir voisin que l'on ai trouvé (c'est à dire le voisin qui partage cette arrète)
+function findNextTile(tiling, newBounds, initiale, currentTile, currentVertexIndex, lastTile, nextTile, bounds, neighbors, currentNeighbor, margeErreur, positionToId){
+	//d'abord on prend un voisin si c'est le premier ou on prend le suivant si on en a deja un
+	if (currentNeighbor == -1){
+		currentNeighbor = coordVertex(tiling, currentTile, neighborVertex(tiling, currentTile, currentVertexIndex, 1));
+		neighbors.push(positionToId.get(currentTile.toString()+","+ neighborVertex(tiling, currentTile, currentVertexIndex, 1).toString()));
+	}
+	else{
+		if (dualDistance(coordVertex(tiling, currentTile, neighborVertex(tiling, currentTile, currentVertexIndex, -1)),currentNeighbor)<margeErreur){
+			currentNeighbor = coordVertex(tiling, currentTile, neighborVertex(tiling, currentTile, currentVertexIndex, 1));
+			neighbors.push(positionToId.get(currentTile.toString()+","+ neighborVertex(tiling, currentTile, currentVertexIndex, 1).toString()));
+		}
+		else if (dualDistance(coordVertex(tiling, currentTile, neighborVertex(tiling, currentTile, currentVertexIndex, 1)),currentNeighbor)<margeErreur){
+			currentNeighbor = coordVertex(tiling, currentTile, neighborVertex(tiling, currentTile, currentVertexIndex, -1));
+			neighbors.push(positionToId.get(currentTile.toString()+","+ neighborVertex(tiling, currentTile, currentVertexIndex, -1).toString()));
+		}
+	}
+	
+	//on cherche la tuile contenant les deux sommet
+	for(var k=0; k<tiling.tiles[currentTile].neighbors.length; k++){
+		nextTile = tiling.tiles[tiling.tiles[currentTile].neighbors[k]].id;
+			
+		//on vérifie que la nouvell tuile n'est pas celle d'ou l'on vient
+		if (lastTile != -1){
+			if (lastTile == nextTile){continue;}
+		}
+				
+		//si on trouve le sommet dans cette tuile on le retient et on cherche la tuile suivante
+		for(var l=0; l<tiling.tiles[nextTile].bounds.length/2; l++){
+			if (dualDistance(coordVertex(tiling, currentTile, currentVertexIndex),coordVertex(tiling, nextTile, l))<margeErreur){
+				if (dualDistance(coordVertex(tiling, nextTile, neighborVertex(tiling, nextTile, l, -1)),currentNeighbor)<margeErreur
+			||dualDistance(coordVertex(tiling, nextTile, neighborVertex(tiling, nextTile, l, 1)),currentNeighbor)<margeErreur){
+					bounds.push(newBounds[2*nextTile], newBounds[2*nextTile + 1]);
+					lastTile = currentTile;
+					currentTile = nextTile;
+					currentVertexIndex = l;
+					//si on a fait un tour complet par rapport au sommt on sort de la recursion
+					if (currentTile == initiale){
+						return true
+					}
+					return findNextTile(tiling, newBounds, initiale, currentTile, currentVertexIndex, lastTile, nextTile, bounds, neighbors, currentNeighbor, margeErreur, positionToId)
+				}
+			}
+		}
+	}
+	return false
+}
+
+//calcule les centres de chaques tuile qui serviront de sommet pour les nouvelles tuiles
+function createNewBounds(tiling){
+	let newBounds =[];
+	for(var i=0; i<tiling.tiles.length; i++){
+		let meanX = 0;
+		let meanY = 0;
+		let count =0;
+    for(var j=0; j<tiling.tiles[i].bounds.length/2; j++){
+			meanX += tiling.tiles[i].bounds[2*j];
+			meanY += tiling.tiles[i].bounds[2*j + 1];
+			count+= 1
+		}
+    newBounds.push(meanX/count, meanY/count);
+  }
+	return newBounds
+}
+
+//rempli le dictionnaire positionToId avec les id correspondant
+function createId(tiling, margeErreur, dico){
+	let newId = [];
+	let count = 0;
+	
+	for(var i=0; i<tiling.tiles.length; i++){
+		for(var j=0; j<tiling.tiles[i].bounds.length/2; j++){
+			let id = idProche(i, j, newId, tiling, margeErreur, dico);
+			if (!id){
+				newId.push([i,j]);
+				dico.set(i.toString()+","+j.toString(), count);
+				count += 1;
+			}
+			else{
+				dico.set(i.toString()+","+j.toString(), id);
+			}
+		}
+	}
+	return
+}
+
+//renvoiele sommet voisin a vertex dans tile en tournant d un ou -1 pas en fonction de différence
+function neighborVertex(tiling, tile, vertex, difference){
+	return (vertex + difference + tiling.tiles[tile].bounds.length/2)%(tiling.tiles[tile].bounds.length/2);
+}
+
+//vérifie si un sommt à deja été exploité (a deja une tuile dans le nouveau pavage)
+function nonParcouru(i,j, parcouru, tiling, margeErreur){
+	for(var k=0; k<parcouru.length; k++){
+		if (dualDistance(coordVertex(tiling, i, j),parcouru[k])<margeErreur){
+			return false
+		}
+	}
+	return true
+}
+
+//vérifie si un sommt à deja été identifié si oui renvoie son id sinon false
+function idProche(i,j, listeId, tiling, margeErreur, dico){
+	for(var k=0; k<listeId.length; k++){
+		if (dualDistance(coordVertex(tiling, i, j),coordVertex(tiling, listeId[k][0], listeId[k][1]))<margeErreur){
+			return dico.get(listeId[k][0].toString()+","+listeId[k][1].toString());
+		}
+	}
+	return false
+}
+
+//renvoie les coordoné du sommt j de la tuile i
+function coordVertex(tiling, i, j){
+	return [tiling.tiles[i].bounds[2*j], tiling.tiles[i].bounds[2*j +1]];
+}
+
+//calcule la distance entre deux sommets en norme infini
+function dualDistance(coord1, coord2){
+  let sq = 0;
+  for(var i=0; i<coord1.length; i++){
+    sq += Math.abs(coord1[i] - coord2[i]);
+  }
+  return sq
+}
