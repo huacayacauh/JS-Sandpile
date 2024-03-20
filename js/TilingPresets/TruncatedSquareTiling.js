e396f05ad1a01a740416cc75e7842bdee8878ae5
Quentin P <60326827+Quentin15@users.noreply.github.com>
2024-03-20 19:15:54 +0100

    Create TruncatedSquareTiling.js

---
+++ js/TilingPresets/TruncatedSquareTiling.js
@@ -0,0 +1,115 @@
+function octagon(x, y){
+	var id = [x, y, 'octagon'];
+
+	var neighbors = [];
+	neighbors.push([x+3, y, 'square']);
+	neighbors.push([x+3, y+3, 'octagon']);
+	neighbors.push([x, y+3, 'square']);
+	neighbors.push([x-3, y+3, 'octagon']);
+    neighbors.push([x-3, y, 'square']);
+	neighbors.push([x-3, y-3, 'octagon']);
+	neighbors.push([x, y-3, 'square']);
+	neighbors.push([x+3, y-3, 'octagon']);
+
+	var bounds = [];
+	bounds.push(x-1, y+2);
+	bounds.push(x+1, y+2);
+	bounds.push(x+2, y+1);
+	bounds.push(x+2, y-1);
+    bounds.push(x+1, y-2);
+	bounds.push(x-1, y-2);
+	bounds.push(x-2, y-1);
+	bounds.push(x-2, y+1);
+
+	return new Tile(id, neighbors, bounds, 8);
+}
+
+function square(x, y){
+    var id = [x, y, 'square'];
+
+    var neighbors = [];
+    neighbors.push([x-3, y, 'octagon']);
+	neighbors.push([x, y+3, 'octagon']);
+	neighbors.push([x+3, y, 'octagon']);
+	neighbors.push([x, y-3, 'octagon']);
+
+    var bounds = [];
+    bounds.push(x-1, y+1);
+    bounds.push(x+1, y+1);
+    bounds.push(x+1, y-1);
+    bounds.push(x-1, y-1);
+
+	return new Tile(id, neighbors, bounds, 4);
+}
+
+Tiling.truncSqTiling = function({size} = {}){
+	var tiles = [];
+
+  tiles.push(octagon(0, 0));
+
+	if (size > 0){
+		for (let i = 1; i<=size; i++){
+			tiles.push(octagon(-3*i, 3*i));
+			tiles.push(octagon(3*i, 3*i));
+			tiles.push(octagon(3*i, -3*i));
+			tiles.push(octagon(-3*i, -3*i));
+		}
+
+		// up
+		for (let i = 1; i<=size; i++){
+			tiles.push(square(-3*i + 3, 3*i));
+			for (j = 1; j<=i-1; j++){
+				tiles.push(square(-3*i + 3 + 6*j, 3*i));
+			}
+		}
+		for (let i = 2; i<=size; i++){
+			for (j = 1; j<= i-1; j++){
+				tiles.push(octagon(-3*i + 6*j, 3*i));
+			}
+		}
+
+		// down
+		for (let i = 1; i<=size; i++){
+			tiles.push(square(-3*i + 3, -3*i));
+			for (j = 1; j<=i-1; j++){
+				tiles.push(square(-3*i + 3 + 6*j, -3*i));
+			}
+		}
+
+		for (let i = 2; i<=size; i++){
+			for (j = 1; j<= i-1; j++){
+				tiles.push(octagon(-3*i + 6*j, -3*i));
+			}
+		}
+
+		// left
+		for (let i = 1; i<=size; i++){
+			tiles.push(square(-3*i, 3*i -3));
+			for (j = 1; j<=i-1; j++){
+				tiles.push(square(-3*i, 3*i - 3 - 6*j));
+			}
+		}
+
+		for (let i = 2; i<=size; i++){
+			for (j = 1; j<= i-1; j++){
+				tiles.push(octagon(-3*i, 3*i-6*j));
+			}
+		}
+
+		// right
+		for (let i = 1; i<=size; i++){
+			tiles.push(square(3*i, 3*i-3));
+			for (j = 1; j<=i-1; j++){
+				tiles.push(square(3*i, 3*i-3-6*j));
+			}
+		}
+
+		for (let i = 2; i<=size; i++){
+			for (j = 1; j<= i-1; j++){
+				tiles.push(octagon(3*i, 3*i - 6*j));
+			}
+		}
+	}
+
+	return new Tiling(tiles);
+}
