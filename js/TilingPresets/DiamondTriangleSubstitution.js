606da3dd3e12ccfd995999af113c767644a2d7c7
Quentin P <60326827+Quentin15@users.noreply.github.com>
2024-04-10 07:49:46 +0200

    Update DiamondTriangleSubstitution.js

--- js/TilingPresets/DiamondTriangleSubstitution.js
+++ js/TilingPresets/DiamondTriangleSubstitution.js
@@ -275,7 +275,7 @@ decorate.set('PARALLELOGRAMME', 1);
 //
 // [7.1] construct "Diamond triangle by subst" tiling by substitution
 // 
-Tiling.DiamondTriangle = function({iterations} = {}){
+Tiling.DiamondTriangleBySubst = function({iterations} = {}){
 	var tiles = [];
 	
 	let tile_1 = PARALLELOGRAMME();
