ce556e31ff7b4cb15e4dc6513043495507385abb
Quentin P <60326827+Quentin15@users.noreply.github.com>
2024-04-22 13:50:49 +0200

    Update Geometry.js
    Add new functions for neighbors

--- js/Utils/Geometry.js
+++ js/Utils/Geometry.js
@@ -46,3 +46,46 @@ function distancePointSegment(x,y,xA,yA,xB,yB){
   return res;
 }
 
+function segmentSlope(segment){
+  let deltaX1 = segment[2] - segment[0];
+  let deltaY1 = segment[3] - segment[1];
+
+  if (Math.abs(deltaX1) < p_error)
+    return Number.POSITIVE_INFINITY;
+  else if (Math.abs(deltaY1) < p_error)
+    return 0;
+  else
+    return deltaY1 / deltaX1;
+}
+
+// If two segments [AB] and [CD] share just one extremity or nothing => not neighbors
+function badCondition(xA, yA, xB, yB, xC, yC, xD, yD){
+  let distBToCD = distancePointSegment(xB, yB, xC, yC, xD, yD);
+  let distDToCD = distancePointSegment(xD, yD, xA, yA, xB, yB);
+  let distAToCD = distancePointSegment(xA, yA, xC, yC, xD, yD);
+  let distCToAB = distancePointSegment(xC, yC, xA, yA, xB, yB);
+  let distDToAB = distancePointSegment(xD, yD, xA, yA, xB, yB);
+  let shareOnExtremity =  distBToCD < p_error && 
+    distance(xB, yB, xC, yC) < p_error &&  
+    distDToCD >= p_error && 
+    distAToCD >= p_error;
+
+  let shareNothing = distAToCD >= p_error &&
+    distBToCD >= p_error &&
+    distCToAB >= p_error &&
+    distDToAB >= p_error;
+
+  return shareOnExtremity || shareNothing;
+  
+}
+
+// Except sharing just on point (an extremity)
+function segmentOnAnother(xA1, yA1, xB1, yB1, xA2, yA2, xB2, yB2, coeff){
+
+  // same segments
+  if(distance(xA1, yA1, xA2, yA2)<p_error && distance(xB1, yB1,xB2, yB2)<p_error)
+    return true;
+
+  return !badCondition(xA1, yA1, xB1, yB1, xA2, yA2, xB2, yB2) && !badCondition(xA2, yA2, xB2, yB2, xA1, yA1, xB1, yB1);
+}
+
