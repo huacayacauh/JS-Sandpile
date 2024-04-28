62078c311094bf6dbe7e1da4a74000071575918c
Quentin P <60326827+Quentin15@users.noreply.github.com>
2024-04-29 01:18:40 +0200

    Update ChangeDisplay.js

--- js/Features/ChangeDisplay.js
+++ js/Features/ChangeDisplay.js
@@ -336,7 +336,7 @@ function hideParams(){
 	func = "Tiling." + func +".toString()";
 	var func_str = eval(func);
 	
-	var params = ["height", "width", "iterations", "size", "order", "cropMethod", "kwidth", "knotchA", "knotchB", "lineplace", "linespace", "kposi", "kposlist", "subneighborhood", "neighbor"];
+	var params = ["height", "width", "iterations", "size", "order", "cropMethod", "kwidth", "knotchA", "knotchB", "lineplace", "linespace", "kposi", "kposlist", "subneighborhood", "neighborFunc"];
 	for(var i=0; i<params.length; i++){
 		if(func_str.includes(params[i])){
 			document.getElementById("p_" + params[i]).style="display:contents";
