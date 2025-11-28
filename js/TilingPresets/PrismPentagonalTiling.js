9cc8592552613fec4e7951fffa379ca33888fdbc
victor <victor.lutfalla@lis-lab.fr>
2025-11-28 15:35:50 +0100

    cleaned up PrismPentagonal

--- js/TilingPresets/PrismPentagonalTiling.js
+++ js/TilingPresets/PrismPentagonalTiling.js
@@ -1,30 +1,32 @@
+// Prismatic pentagon tiling
+// Periodic tiling with a single pentagonal tile (up to isometry)
+// FIXME : add a better description, maybe a reference to another source for this tiling
+//
 Tiling.prismPentTiling = function ({size}={}) { //number of lines of neighbors
-	var tils = [];
+    var tils = [];
     if(size >= 1)
         pushUpDownPent(0,0, tils); //central up and down pentagons
-
+    
     for(var i = 1; i < size; i++){
         // left and right pentagons on line n° i (y = 0)
         pushUpDownPent(i, 0, tils);
         pushUpDownPent(-i, 0, tils);
-
-		for(var j = 1; j < i; j++){
+	
+	for(var j = 1; j < i; j++){
             // diagonals pentagons on line n° i
             pushUpDownPent(i - 0.5*j, 2.25*j, tils);
             pushUpDownPent(i - 0.5*j, -2.25*j, tils);
             pushUpDownPent(-i + 0.5*j, 2.25*j, tils);
             pushUpDownPent(-i + 0.5*j, -2.25*j, tils);
-		}
-
+	}
+	
         for (var k = 0 ; k <= i; k++){
             // top pentagons on line n° i
             pushUpDownPent(-0.5*i + k, 2.25*i, tils);
             pushUpDownPent(-0.5*i + k, -2.25*i, tils);
-        }
-        
-	}
-	
-	return new Tiling(tils);
+        }    
+    }
+    return new Tiling(tils);
 }
 
 function pushUpDownPent(x, y, tils){
@@ -33,43 +35,35 @@ function pushUpDownPent(x, y, tils){
 }
 
 function upPent(x, y){
-	var id = [x, y];
-	
-	var neighbors = [];
-	
-	neighbors.push([x+1, y]); 
-	neighbors.push([x+0.5, y+1.25]);
+    var id = [x, y];	
+    var neighbors = [];
+    neighbors.push([x+1, y]); 
+    neighbors.push([x+0.5, y+1.25]);
     neighbors.push([x-0.5, y+1.25]);
-	neighbors.push([x-1, y]);
-	neighbors.push([x, y-1]);
-	
-	var bounds = [];
+    neighbors.push([x-1, y]);
+    neighbors.push([x, y-1]); 
+    var bounds = [];
     bounds.push(x+0.5, y-0.5);
     bounds.push(x+0.5, y+0.5);
     bounds.push(x, y+0.75);
     bounds.push(x-0.5, y+0.5);
-    bounds.push(x-0.5, y-0.5);
-				
-	return new Tile(id, neighbors, bounds, 5);
+    bounds.push(x-0.5, y-0.5);    
+    return new Tile(id, neighbors, bounds, 5);
 }
 
 function downPent(x, y){
-	var id = [x, y];
-	
-	var neighbors = [];
-	
-	neighbors.push([x+1, y]);
-	neighbors.push([x, y+1]);
+    var id = [x, y];   
+    var neighbors = [];   
+    neighbors.push([x+1, y]);
+    neighbors.push([x, y+1]);
     neighbors.push([x-1, y]);
-	neighbors.push([x-0.5, y-1.25]);
-	neighbors.push([x+0.5, y-1.25]);
-	
-	var bounds = [];
+    neighbors.push([x-0.5, y-1.25]);
+    neighbors.push([x+0.5, y-1.25]);   
+    var bounds = [];
     bounds.push(x+0.5, y+0.5);
     bounds.push(x-0.5, y+0.5);
     bounds.push(x-0.5, y-0.5);
     bounds.push(x, y-0.75);
-    bounds.push(x+0.5, y-0.5);
-				
-	return new Tile(id, neighbors, bounds, 5);
+    bounds.push(x+0.5, y-0.5);  
+    return new Tile(id, neighbors, bounds, 5);
 }
